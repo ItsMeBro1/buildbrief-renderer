363de0a71b96bf3f0b5e36757d4265fdce15daeb
ItsMeBro1 <dampa266@gmail.com>
2026-03-22 12:02:55 +0100

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+});
 
 app.post('/render', async (req, res) => {
     const { voxels, width, height, length } = req.body;
